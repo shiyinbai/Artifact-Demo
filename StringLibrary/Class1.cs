a274ba0f5e9735c77cde47ba12a05d42025162ef
shiyin <shiyin.bai@elca.ch>
2022-07-05 10:49:00 +0200

    initialize project

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace StringLibrary;
+public static class StringOperations
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+
+    public static bool StartsWithLower(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsLower(ch);
+    }
+}
